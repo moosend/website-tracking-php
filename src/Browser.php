c9d5805bc9f478ad5b00ab5bd74e676c7bce49c2
Gent Spahiu <gentsp@moosend.com>
2019-12-02 17:45:06 +0100

    Check if HTTP_USER_AGENT is not empty when getting the Agent info

--- src/Browser.php
+++ src/Browser.php
@@ -10,7 +10,7 @@ class Browser
     public static function getUserAgent()
     {
 
-        return $_SERVER['HTTP_USER_AGENT'];
+        return !empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
     }
 
     public static function getRequestIPAddress()
